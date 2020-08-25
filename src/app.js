18794a389295d2783fcca4ff3cf7bc755c39264f
avijit1998 <avijitnanda14@gmail.com>
2020-08-25 20:16:20 +0530

    serving html and json

--- src/app.js
+++ src/app.js
@@ -3,19 +3,25 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello Express!");
+  res.send("<h1>Weather App</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help Page!");
+  res.send([
+    { name: "Avijit", age: 22 },
+    { name: "Andrew", age: 27 },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page!");
+  res.send("<h1>About Page!</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({
+    forecast: "Cloudy with chances of rain",
+    location: "Bhubaneswar",
+  });
 });
 
 app.listen(3000, () => {
